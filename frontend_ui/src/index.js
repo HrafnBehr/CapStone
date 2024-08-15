73d7b3edd0b6e4357933a4b8d736d85c9f0b0cc4
Jason Bullard <jason@bullard.io>
2024-08-14 19:27:06 -0500

    fix: use MUI styles using CssBaseline

--- frontend_ui/src/index.js
+++ frontend_ui/src/index.js
@@ -1,8 +1,12 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import ReactDOM from 'react-dom/client'
-import './index.css'
-
 import App from './App'
+import { CssBaseline } from '@mui/material'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(<App />)
+root.render(
+  <Fragment>
+    <CssBaseline />
+    <App />
+  </Fragment>
+)
