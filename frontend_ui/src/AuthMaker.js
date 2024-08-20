52c477012fe037e908d587e4cbd8b049f8472e06
Jason Bullard <jason@bullard.io>
2024-08-20 10:04:15 -0500

    get user info after resolving auth state

--- frontend_ui/src/AuthMaker.js
+++ frontend_ui/src/AuthMaker.js
@@ -11,21 +11,21 @@ export const AuthContext = createContext({
 export const AuthContextProvider = ({ children }) => {
   const [isAuthenticated, setIsAuthenticated] = useState(false)
   const [loading, setLoading] = useState(true)
-  const [user, setUser] = useState({})
+  const [user, setUser] = useState()
 
   useEffect(() => {
     checkAuth().then(async (ok) => {
-      // if (ok) {
-      //   const res = await fetch(
-      //     `http://localhost:8080/api/v1/users/getUserInfo`,
-      //     {
-      //       credentials: 'include',
-      //     },
-      //   )
+      if (ok) {
+        const res = await fetch(
+          `http://localhost:8080/api/v1/users/getUserInfo`,
+          {
+            credentials: 'include',
+          },
+        )
 
-      //   const data = await res.json()
-      //   setUser(data.user)
-      // }
+        const data = await res.json()
+        setUser(data.user)
+      }
       setIsAuthenticated(ok)
       setLoading(false)
     })
