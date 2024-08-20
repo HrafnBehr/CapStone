7781a2a6e0fbc5caa542410fa27342e1d9f47a74
Jason Bullard <jason@bullard.io>
2024-08-20 16:10:58 -0500

    update expiration date

--- backend_server/src/models/auth/auth.routes.js
+++ backend_server/src/models/auth/auth.routes.js
@@ -21,12 +21,12 @@ router.post('/login', async (req, res) => {
         httpOnly: true,
         secure: process.env.NODE_ENV === 'production',
         sameSite: 'strict',
-        maxAge: 60 * 60 * 24 * 14, // 2 weeks
+        maxAge: 24 * 60 * 60 * 1000 * 14, // 2 weeks
       })
       .status(200)
       .send()
   } catch (err) {
-    return res.clearCookie('jwt').status(401).json({ error: err.message })
+    return res.status(401).json({ error: err.message })
   }
 })
 
@@ -37,12 +37,12 @@ router.post('/logout', async (_req, res) => {
 router.get('/verify', async (req, res) => {
   const token = req.cookies.jwt
   if (!token) {
-    return res.clearCookie('jwt').status(401).send()
+    return res.status(401).send()
   }
 
   jwt.verify(token, process.env.JWT_SECRET, (err) => {
     if (err) {
-      return res.clearCookie('jwt').status(401).send()
+      return res.status(401).send()
     }
 
     return res.status(200).send()
