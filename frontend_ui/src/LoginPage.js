9aed92af62071fd8a89c5a9c20eb3a6207d2ce09
Jason Bullard <jason@bullard.io>
2024-08-20 13:38:27 -0500

    refactor login page

--- frontend_ui/src/LoginPage.js
+++ frontend_ui/src/LoginPage.js
@@ -2,24 +2,20 @@ import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import {
   Container,
-  Card,
   TextField,
   Button,
-  CardContent,
   Stack,
   FormControlLabel,
   Checkbox,
   Box,
-  CardHeader,
   Typography,
+  Divider,
 } from '@mui/material'
+import { useToast } from './hooks/useToast'
 import { useAuth } from './hooks/useAuth'
 import { login } from './api/users'
-import { useToast } from './hooks/useToast'
 
 export default function Login() {
-  const [username, setUsername] = useState('')
-  const [password, setPassword] = useState('')
   const [loading, setLoading] = useState(false)
 
   const toast = useToast()
@@ -29,6 +25,10 @@ export default function Login() {
   const handleLogin = async (e) => {
     e.preventDefault()
     setLoading(true)
+
+    const formData = new FormData(e.target)
+    const username = formData.get('username')
+    const password = formData.get('password')
 
     try {
       const isLoggedIn = await login({ username, password })
@@ -56,46 +56,46 @@ export default function Login() {
 
   return (
     <>
-      <Container maxWidth='sm'>
-        <Box
-          sx={{
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-            height: '100vh',
-          }}
-          as='form'
+      <Container
+        maxWidth='md'
+        sx={{
+          minHeight: '100vh',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        <Stack
+          flex={1}
+          direction='row'
+          justifyContent='center'
+          alignItems='center'
+          sx={{ backgroundColor: '#005e7c', boxShadow: 2 }}
         >
-          <Card variant='outlined' sx={{ width: '100%' }}>
-            <CardHeader
-              title={
-                <Typography
-                  variant='h4'
-                  component='h1'
-                  sx={{ textAlign: 'center' }}
-                >
-                  Login
-                </Typography>
-              }
-            />
+          <Box sx={{ backgroundColor: 'white', width: '100%' }}>
+            <Stack p={4} spacing={2} as='form' onSubmit={handleLogin}>
+              <Typography
+                variant='h4'
+                component='h1'
+                color='primary.main'
+                textAlign='center'
+              >
+                Login to your account
+              </Typography>
+              <Divider />
 
-            <CardContent>
-              <Stack gap={2}>
+              <Stack spacing={2}>
                 <TextField
-                  id='username-login'
                   label='Username'
-                  variant='outlined'
-                  onChange={(e) => setUsername(e.target.value)}
+                  name='username'
                   autoComplete='username'
                   fullWidth
                 />
 
                 <TextField
-                  id='username-password'
                   type='password'
                   label='Password'
-                  variant='outlined'
-                  onChange={(e) => setPassword(e.target.value)}
+                  name='password'
                   autoComplete='current-password'
                   fullWidth
                 />
@@ -106,19 +106,115 @@ export default function Login() {
                   sx={{ width: 'fit-content' }}
                 />
 
+                <Button variant='contained' type='submit' disabled={loading}>
+                  {loading ? 'Logging in...' : 'Sign In'}
+                </Button>
+              </Stack>
+            </Stack>
+          </Box>
+
+          <Box sx={{ width: '100%' }}>
+            <Stack alignItems='center' justifyContent='center' color='white'>
+              <Typography variant='h5' component='h2'>
+                Welcome to PROMPT
+              </Typography>
+              <Typography variant='body1'>Begin Your Journey Here</Typography>
+              <Button
+                variant='outlined'
+                color='inherit'
+                onClick={() => navigate('/register')}
+                sx={{ mt: 3 }}
+              >
+                Register
+              </Button>
+            </Stack>
+          </Box>
+        </Stack>
+      </Container>
+
+      {/*       <Container maxWidth='lg'>
+        <Box className='login-form'>
+          <Box className='left-section'>
+            <Typography variant='h4' component='h1' className='login-title'>
+              Login to Your Account
+            </Typography>
+            <Stack
+              direction='row'
+              spacing={2}
+              justifyContent='center'
+              className='dark-mode-toggle'
+            >
+              <Typography variant='body2' className='dark-mode-text'>
+                Try out our dark mode
+              </Typography>
+              <IconButton onClick={toggleDarkMode} color='inherit'>
+                {darkMode ? <Brightness7 /> : <Brightness4 />}
+              </IconButton>
+            </Stack>
+            <Divider className='divider' />
+            <form onSubmit={handleLogin} style={{ marginTop: '20px' }}>
+              <Stack spacing={2}>
+                <TextField
+                  id='username-login'
+                  label='Username'
+                  variant='outlined'
+                  value={username}
+                  onChange={(e) => setUsername(e.target.value)}
+                  autoComplete='username'
+                  fullWidth
+                  className='login-input'
+                />
+
+                <TextField
+                  id='username-password'
+                  type='password'
+                  label='Password'
+                  variant='outlined'
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  autoComplete='current-password'
+                  fullWidth
+                  className='login-input'
+                />
+
+                <FormControlLabel
+                  control={
+                    <Checkbox defaultChecked className='login-checkbox' />
+                  }
+                  label='Remember Me'
+                  sx={{ width: 'fit-content', color: '#005E7C' }}
+                />
+
                 <Button
                   variant='contained'
                   type='submit'
-                  onClick={handleLogin}
                   disabled={loading}
+                  className='login-button'
                 >
-                  {loading ? 'Logging in...' : 'Login'}
+                  {loading ? 'Logging in...' : 'Sign In'}
                 </Button>
               </Stack>
-            </CardContent>
-          </Card>
+            </form>
+          </Box>
+
+          <Box className='right-section'>
+            <Typography variant='h5' component='h2' className='signup-title'>
+              Welcome to PROMPT
+            </Typography>
+            <Typography variant='body1' className='signup-subtitle'>
+              Begin Your Journey Here
+            </Typography>
+            <Button
+              variant='outlined'
+              color='secondary'
+              onClick={() => navigate('/register')}
+              className='signup-button'
+            >
+              Sign Up
+            </Button>
+          </Box>
         </Box>
-      </Container>
+      </Container> */}
     </>
   )
 }
