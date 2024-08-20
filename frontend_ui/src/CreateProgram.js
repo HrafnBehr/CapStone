90c160be2c0d4ef210161703fd5c7f5c54b04933
Jason Bullard <jason@bullard.io>
2024-08-20 12:08:36 -0500

    refactor CreateProgram

--- frontend_ui/src/CreateProgram.js
+++ frontend_ui/src/CreateProgram.js
@@ -8,57 +8,56 @@ import {
   Button,
   CardContent,
   Stack,
-  FormControl,
 } from '@mui/material'
 import { DatePicker } from '@mui/x-date-pickers/DatePicker'
-import { LocalizationProvider } from '@mui/x-date-pickers'
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
 import dayjs from 'dayjs'
 import { PathwaySelect } from './components/PathwaySelect'
 import { ProjectManagerSelect } from './components/ProjectManagerSelect'
+import { useToast } from './hooks/useToast'
 
 export default function CreateProgram() {
-  // enables navigate
   const navigate = useNavigate()
+  const toast = useToast()
 
   const [projectDetails, setProjectDetails] = useState({
     name: '',
-    start_date: new Date(),
-    end_date: new Date(),
+    start_date: dayjs(),
+    end_date: dayjs(),
     description: '',
     pathway_id: null,
     project_manager_id: null,
   })
 
-  // our async addProgram which handles adding a program to the ui by fetching the endpoint data
-  const addProgram = async (e) => {
-    // prevents default action from being taken unless explicitly done so
+  const handleSubmit = async (e) => {
     e.preventDefault()
 
-    // try hook that leads into our fetch
+    const formData = new FormData(e.target)
+    const programDetails = {
+      name: formData.get('name'),
+      start_date: formData.get('start_date'),
+      end_date: formData.get('end_date'),
+      description: formData.get('description'),
+      pathway_id: formData.get('pathway_id'),
+      project_manager_id: formData.get('project_manager_id'),
+    }
+
     try {
-      // our fetch reaching out to our data endpoint and what it is communicating
       const response = await fetch('http://localhost:8080/api/v1/projects', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
         },
-
-        // turning the data of programToBeMade into string data for the database
-        body: JSON.stringify(projectDetails),
+        body: JSON.stringify(programDetails),
       })
 
       if (!response.ok) {
         throw new Error('failed to fulfill your request')
       }
 
-      alert('New program added to listing')
+      toast.success('New program added to listing')
       navigate('/')
-
-      // catch hook for our above try
     } catch (error) {
-      // error message for us to quickly identify where our code is skitzing out
-      console.error('Error upon adding program: check lines 42-60', error)
+      toast.error(error.message)
     }
   }
 
@@ -69,65 +68,31 @@ export default function CreateProgram() {
           <CardContent>
             <h1>Create A Project</h1>
 
-            <form onSubmit={addProgram}>
+            <form onSubmit={handleSubmit}>
               <Stack gap={2}>
-                <FormControl>
-                  <TextField
-                    id='project-name'
-                    label='Project Name'
-                    variant='outlined'
-                    onChange={(e) =>
-                      setProjectDetails({
-                        ...projectDetails,
-                        name: e.target.value,
-                      })
-                    }
-                  />
-                </FormControl>
+                <TextField id='name' name='name' label='Project Name' />
 
-                <FormControl>
-                  <TextField
-                    id='project-description'
-                    label='Project Description'
-                    variant='outlined'
-                    multiline
-                    rows={5}
-                    onChange={(e) =>
-                      setProjectDetails({
-                        ...projectDetails,
-                        description: e.target.value,
-                      })
-                    }
-                  />
-                </FormControl>
+                <TextField
+                  id='description'
+                  name='description'
+                  label='Project Description'
+                  multiline
+                  rows={5}
+                />
 
-                <LocalizationProvider dateAdapter={AdapterDayjs}>
-                  <DatePicker
-                    label='Start Date'
-                    id='start_date'
-                    defaultValue={dayjs(projectDetails.start_date)}
-                    onChange={(e) =>
-                      setProjectDetails({
-                        ...projectDetails,
-                        start_date: dayjs(e).valueOf(),
-                      })
-                    }
-                  />
-                </LocalizationProvider>
+                <DatePicker
+                  label='Start Date'
+                  id='start_date'
+                  name='start_date'
+                  defaultValue={dayjs()}
+                />
 
-                <LocalizationProvider dateAdapter={AdapterDayjs}>
-                  <DatePicker
-                    label='End Date'
-                    id='end_date'
-                    defaultValue={dayjs(projectDetails.end_date)}
-                    onChange={(e) =>
-                      setProjectDetails({
-                        ...projectDetails,
-                        end_date: dayjs(e).valueOf(),
-                      })
-                    }
-                  />
-                </LocalizationProvider>
+                <DatePicker
+                  label='End Date'
+                  id='end_date'
+                  name='end_date'
+                  defaultValue={dayjs()}
+                />
 
                 <PathwaySelect
                   pathwayId={projectDetails.pathway_id}
@@ -149,17 +114,13 @@ export default function CreateProgram() {
                   }}
                 />
 
-                <Button
-                  variant='contained'
-                  type='submit'
-                  onClick={() => addProgram}
-                >
+                <Button variant='contained' type='submit'>
                   Create
                 </Button>
                 <Button
                   variant='outlined'
                   type='button'
-                  onClick={() => navigate('/Home')}
+                  onClick={() => navigate('/')}
                 >
                   Back
                 </Button>
